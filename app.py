5c27d74d07d4cfcabb5a4fcab204a86bda4e014a
Rui Shan <ruishan.foo@gmail.com>
2020-11-01 22:22:07 +0800

    connecting to route

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ import os
 
 app = Flask(__name__)
 
-
+wsgi_app = app.wsgi_app
 
 # "magic code" -- boilerplate
-if __name__ == '__main__':
+#if __name__ == '__main__':
     # app.run(host=os.environ.get('IP'),     #must give a host (IP address)
     #         port=int(os.environ.get('PORT')),   #networking clients access
     #         debug=True)
-    app.run(host="localhost",     #must give a host (IP address)
-            port=8080,   #networking clients access
-            debug=True)
+if __name__ == '__main__': 
+  
+    # run() method of Flask class runs the application  
+    # on the local development server. 
+    app.run()
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return "It's working!"
