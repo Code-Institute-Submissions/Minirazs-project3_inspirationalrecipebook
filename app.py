9f70a366cfcc153f24e8fe9035f60936a835c8b8
Rui Shan <ruishan.foo@gmail.com>
2020-11-03 20:16:21 +0800

    added recipe template html files

--- app.py
+++ app.py
@@ -2,19 +2,31 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import os
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-# from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+
 if os.path.exists("env.py"):
     import env
 
+load_dotenv()
+
+# declare the global variables to store the URL to the Mongo database
+MONGO_URL = os.environ.get('MONGO_URL')
+DB_NAME = "recipesbook"
+
+# create the Mongo client
+client = pymongo.MongoClient(MONGO_URL)
+# as db variable is outside of every functions, it is a global variable
+# we can use the db variable inside any functions
+db = client[DB_NAME]
 
 app = Flask(__name__)
-
+app.secret_key = os.environ.get('SECRET_KEY')
 
 @app.route('/')
 def home():
     return "It's working!"
 
-
+# route to show all recipes
 @app.route('/recipes')
 def show_recipes():
     all_recipes = db.recipes.find()
