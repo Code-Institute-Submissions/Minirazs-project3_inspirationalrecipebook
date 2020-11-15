7c9624b8fe75740b820026c89421d829074d1adb
Rui Shan <ruishan.foo@gmail.com>
2020-11-15 16:17:13 +0800

    fixing regex for form input validation

--- js/misc.js
+++
@@ -1,7 +0,0 @@
-    // popovers initialization - on hover
-    $('[data-toggle="popover-hover"]').popover({
-        html: true,
-        trigger: 'hover',
-        placement: 'right',
-        content: function () { return '<img src="' + $(this).data('img') + '" />'; }
-      });
